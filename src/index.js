e99b029d4a0c4fb15bd35ad849c01898e8df1694
Igor Vereshchagin <9868425@gmail.com>
2021-03-15 20:35:55 +0300

    add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+    const   numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const numberToString = number.toString();
+    const lengthNumbers = numberToString.length;
+
+    if (number === 0) {
+        return 'zero';
+    }
+    if (lengthNumbers === 1) {
+        return numbers[number - 1];
+    }
+    if (lengthNumbers === 2 && number < 20) {
+        return numbers[number - 1];
+    } else if (lengthNumbers === 2 && number % 10 === 0) {
+        return tens[numberToString[0] - 1];
+    } else if (lengthNumbers === 2) {
+        return `${tens[numberToString[0] - 1]} ${numbers[numberToString[1] - 1]}`;
+    }
+    if (lengthNumbers  === 3 && +(numberToString.slice(1)) === 0) {
+        return `${numbers[numberToString[0] - 1]} hundred`;
+    } else {
+        return `${numbers[numberToString[0] - 1]} hundred ${toReadable((numberToString.slice(1)))}`;
+    }
 }
+
